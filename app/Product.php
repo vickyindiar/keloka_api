03bb5475bf08a6fcbf6cc1a88f5021a5bed22203
vickyindiar <vickynewonline@gmail.com>
2019-07-18 22:50:08 +0700

    update

--- app/Product.php
+++ app/Product.php
@@ -2,6 +2,10 @@
 
 namespace App;
 
+use App\Qtytype;
+use App\Category;
+use App\Supplier;
+use App\Brand;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
@@ -10,4 +14,21 @@ class Product extends Model
     use SoftDeletes;
     protected $table = 'products';
     protected $fillable = ['name', 'sprice' , 'bprice', 'qtytype_id', 'stock', 'category_id', 'supplier_id', 'color', 'image', 'desc' ];
+
+    public function qtytype(){
+        return $this->belongsTo(Qtytype::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function supplier(){
+        return $this->belongsTo(Supplier::class);
+    }
+
+    public function brand(){
+        return $this->belongsTo(Brand::class);
+    }
+
 }
